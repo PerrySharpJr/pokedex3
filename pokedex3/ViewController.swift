9d2e22d321d43f9fb30f85ce7f5333230e4dfd43
PerrySharpJr <PerrySharpJr@gmail.com>
2018-10-27 19:28:41 -0400

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Perry Sharp on 10/27/18.
+//  Copyright © 2018 Perry Sharp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
